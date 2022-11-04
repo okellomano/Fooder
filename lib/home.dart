7377f388c737865cfe732813dd0d3a0ab56e8c69
Mano <onyandookello@gmail.com>
2022-11-04 11:33:00 +0300

    Bottom  navigation bar

--- lib/home.dart
+++ lib/home.dart
@@ -8,20 +8,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.green,
+    ),
+    Container(
+      color: Colors.blue,
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Fooderlich',
-          style: Theme.of(context).textTheme.headline6,
+        appBar: AppBar(
+          title: Text(
+            'Fooderlich',
+            style: Theme.of(context).textTheme.headline6,
+          ),
         ),
-      ),
-      body: Center(
-          child: Text(
-        'Get Cooking!',
-        style: Theme.of(context).textTheme.headline1,
-      )),
-    );
+        body: pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor:
+              Theme.of(context).textSelectionTheme.selectionColor,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: <BottomNavigationBarItem>[
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card'),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card2'),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card3'),
+          ],
+        ));
   }
 }
